a2efdd5f2590a44a9eca10b96c850a2633b011f9
Ryan Campbell <16215049+Rc1998@users.noreply.github.com>
2020-10-28 14:29:04 -0500

    First implementation of player class.

--- Othello_Project/src/application/Player.java
+++ Othello_Project/src/application/Player.java
@@ -4,19 +4,31 @@ package application;
  */
 public class Player
 {
-	String playerName;
+	private String playerName;
+	private int password;
 
 	/**
 	 * Default constructor
 	 */
-	public Player() 
-	{
-		
+	public Player(String name, int pass) {
+		this.playerName = name;
+		this.password = pass;
+	}
+
+	public boolean isPlayer(String name, int pass) {
+		if (playerName == name && password == pass){
+			return true;
+		} else {
+			return false;
+		}
 	}
 	
-	public String getName()
-	{
+	public String getName() {
 		return playerName;
+	}
+
+	public int getPassword() {
+		return password;
 	}
 
 }
